53f97dd8b23a9d9dfcfc07c2136cfefa886f6f82
giovanna <giovanaaraujolima2019@gmail.com>
2022-03-30 11:40:30 -0300

    ex2php

---
+++ lista2/ex2.php
@@ -0,0 +1,18 @@
+<?php
+    $mes=2;
+    switch($mes){
+    case'1': echo "Janeiro"; break;
+    case'1': echo "Fevereiro"; break;
+    case'1': echo "março"; break;
+    case'1': echo "abril"; break;
+    case'1': echo "maio"; break;
+    case'1': echo "junho"; break;
+    case'1': echo "julho"; break;
+    case'1': echo "agosto"; break;
+    case'1': echo "setembro"; break;
+    case'1': echo "outubro"; break;
+    case'1': echo "novembro"; break;
+    case'1': echo "dezembro"; break;
+    case'1': echo ""; break;
+    }
+?>
