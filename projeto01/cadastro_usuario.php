ba520b76229823f8585943aca7ffba00c7ef142f
giovanna <giovanaaraujolima2019@gmail.com>
2022-04-20 11:04:48 -0300

    ex

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,12 @@
+<?php
+    $nome =$_POST["nome_usuario"];
+    echo "nome: ".$nome;
+    echo '<br>';
+    $email =$_POST["email_usuario"];
+    echo "email: ".$email;
+    echo '<br>';
+    $telefone =$_POST["fone_usuario"];
+    echo "telefone: ".$telefone;
+    echo '<br>';
+
+?>
