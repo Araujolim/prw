d30bc831d19c75ca600be5bece54d1164b6774ec
giovanna <giovanaaraujolima2019@gmail.com>
2022-04-20 11:04:05 -0300

    ex1

--- lista4/ex1.php
+++ lista4/ex1.php
@@ -1,6 +1,22 @@
 <?php
     $operacao=$_POST["operacao"];
-    echo $operacao . "<br>";
     $num1=$_POST["num1"];
-    echo $num1;
+    $num2=$_POST["num2"];
+
+    if($operacao == 0){
+        $soma = $num1 + $num2;
+        echo "soma: ". $soma;
+    }
+    else if($operacao == 1){
+            $sub = $num1 - $num2;
+            echo "subtração: ". $sub;
+        } 
+        else if($operacao == 2){
+                $div = $num1 / $num2;
+                echo "divisão: ". $div;
+            } 
+            else if($operacao == 3){
+                $mult = $num1 * $num2;
+                echo "multiplicação ". $mult;
+            } 
 ?>
