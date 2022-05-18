968aed5e690fdf2a61ae72fdd5bf0bed6516d40a
giovanna <giovanaaraujolima2019@gmail.com>
2022-05-18 09:33:27 -0300

    j

--- projeto01/conexao.php
+++ projeto01/conexao.php
@@ -6,8 +6,8 @@
     $database = "projeto01";
     $con = mysqli_connect($hostname, $username, $password, $database, $port);
     if(mysqli_connect_errno()){
-        printf("erro ao conectar ao banco de dados: %s\n", mysqli_connect_errno());
+        printf("erro ao conectar ao banco de dados: %s\n", mysqli_connect_error());
         exit;
     }
-    //printf("Banco de dados conectados com sucesso \o/");
+    printf("Banco de dados conectados com sucesso \o/");
 ?>
