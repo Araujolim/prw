8765680eca1a8dbc3a7f03cd70f1a15fc8a6ef9d
giovanna <giovanaaraujolima2019@gmail.com>
2022-05-18 09:24:17 -0300

    ex

--- projeto01/lista_usuarios.php
+++ projeto01/lista_usuarios.php
@@ -31,6 +31,8 @@
                 echo "<td>". $row['email_usuario']. "</td>";
                 echo "<td>". $row['telefone_usuario']. "</td>";
                 echo "</td>";
+                echo "<td><a href='excluir_usuario.php?id_usuario=".$row['id_usuario']."'>Excluir</a></td>";
+                echo "</tr>";
             }
         ?>
     </table>
